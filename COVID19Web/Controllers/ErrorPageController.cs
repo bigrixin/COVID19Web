9afccaeefe0b5620a2c8e29777ed2d919047c7df
Steven <steven_zhai@hotmail.com>
2021-08-15 16:57:39 +1000

    add error handle

---
+++ COVID19Web/Controllers/ErrorPageController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace COVID19Web.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public ActionResult Error(int statusCode, Exception exception)
+        {
+            Response.StatusCode = statusCode;
+            ViewBag.StatusCode = statusCode + " Error";
+            return View();
+        }
+    }
+}
--- COVID19Web/Global.asax.cs
+++ COVID19Web/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using COVID19Web.App_Start;
+using COVID19Web.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,5 +21,33 @@ namespace COVID19Web
 
             DependencyConfig.RegisterDependencyResolvers();
         }
+
+        public void Application_Error(Object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "ErrorPage");
+            routeData.Values.Add("action", "Error");
+            routeData.Values.Add("exception", exception);
+
+            if (exception.GetType() == typeof(HttpException))
+            {
+                routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
+            }
+            else
+            {
+                routeData.Values.Add("statusCode", 500);
+            }
+
+            Response.TrySkipIisCustomErrors = true;
+            IController controller = new ErrorPageController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+            Response.End();
+        }
+
     }
+
+
 }
